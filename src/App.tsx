18c7727e9bc01ea39e5a3bd4257fecbaa28155db
Alex Le <thanhdungvn@gmail.com>
2022-12-05 10:13:22 +0700

    save

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,36 @@ import React, { useState } from 'react'
 import './App.css'
 
 // TODO: Please implement this component.
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>LIKE App - test commit</h1>
+//     </div>
+//   )
+// }
+
 const App = () => {
   const [count, setCount] = useState(1)
   return (
-    <button id="like-button" onClick={() => setCount(count + 1)}>
-      {count}
-    </button>
+    <>
+      <p id="like-count">{count}</p>
+      <button id="like-button" onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+    </>
   )
 }
 
 export default App
+
+// const Test = () => {
+//   const [count, setCount] = useState(1)
+//   return (
+//     <>
+//       <p id="like-count">{count}</p>
+//       <button id="like-button" onClick={() => setCount(count + 1)}>
+//         Increase
+//       </button>
+//     </>
+//   )
+// }
